7db0cd168d594b81ab4d50d6a04a5ec59087deed
Sadia2705 <62227594+Sadia2705@users.noreply.github.com>
2021-07-24 18:22:26 +1000

    Add files via upload

---
+++ task1_1p1.cpp
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(13, HIGH);
+    Serial.println("Motion Detected");
+    delay(1000); // Wait for 1000 millisecond(s)
+  } else {
+    digitalWrite(13, LOW);
+    Serial.println("Motion NOT Detected");
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+}
